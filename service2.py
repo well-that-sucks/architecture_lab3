8bb3f04072f27702335afb2f2bb82479bb3e1999
well-that-sucks <55409370+well-that-sucks@users.noreply.github.com>
2021-12-26 19:06:31 +0200

    Added pagination and changed logic to work with DB

--- service2.py
+++ service2.py
@@ -1,27 +1,64 @@
 from flask import Flask
 from flask_restful import Api
+from flask_restful import reqparse
+import pyodbc as db
+
+class Singleton:
+    def __init__(self, cls):
+        self._cls = cls
+
+    def get_instance(self):
+        try:
+            return self._instance
+        except AttributeError:
+            self._instance = self._cls()
+            return self._instance
+
+    def __call__(self):
+        raise TypeError('Singletons must be accessed through `get_instance()`.')
+
+    def __instancecheck__(self, inst):
+        return isinstance(inst, self._cls)
+
+@Singleton
+class DBConnection(object):
+
+    def __init__(self):
+        self.conn = db.connect('Driver={SQL Server};Server=DESKTOP-69AKAQS;Database=Service1;Trusted_Connection=yes;')
+
+    def __str__(self):
+        return self.conn
 
 if __name__ == '__main__':
+
+    conn = DBConnection.get_instance().conn
     app = Flask(__name__)
     api = Api(app)
     @app.route('/price-list/', methods = ['GET'])
     def proc_pricelist():
-        hardcoded_pricelist_itemset = [{'item_id': 101, 'item_name': 'Jack Daniels', 'price': 10},
-                                    {'item_id': 102, 'item_name': 'Captain Morgan Spiced Gold', 'price': 40}, 
-                                    {'item_id': 103, 'item_name': 'Martini Asti', 'price': 20},
-                                    {'item_id': 104, 'item_name': 'Fiorelli Bianco', 'price': 25},
-                                    {'item_id': 105, 'item_name': 'Bacardi Spiced', 'price': 35}]
-        return {'items': hardcoded_pricelist_itemset}
+        parser = reqparse.RequestParser()
+        parser.add_argument('page')
+        parsed_args = parser.parse_args()
+        items_dict = []
+        if parsed_args['page']:
+            min_id = (int(parsed_args['page']) - 1) * 5000 + 100001
+            max_id = int(parsed_args['page']) * 5000 + 100000
+            with conn.cursor() as cursor:
+                cursor.execute(f'SELECT * FROM Prices WHERE ItemID >= {min_id} AND ItemID <= {max_id}')
+                items = cursor.fetchall()
+            for item in items:
+                items_dict.append({'item_id': item[0], 'item_name': item[1], 'price': item[2]})
+        return {'items': items_dict}
     
     @app.route('/details/<int:id>', methods = ['GET'])
     def proc_details(id):
-        hardcoded_details_itemset = [{'item_id': 101, 'description': 'Whiskey', 'quantity': 119, 'supplier_id': 4},
-                            {'item_id': 102, 'description': 'Rome', 'quantity': 54, 'supplier_id': 1}, 
-                            {'item_id': 103, 'description': 'Wine', 'quantity': 99, 'supplier_id': 2},
-                            {'item_id': 104, 'description': 'Fragolino', 'quantity': 5, 'supplier_id': 4},
-                            {'item_id': 105, 'description': 'Rome', 'quantity': 140, 'supplier_id': 5}]
-        for item in hardcoded_details_itemset:
-            if item['item_id'] == id:
-                return item
+        with conn.cursor() as cursor:
+            cursor.execute(f'SELECT * FROM Details WHERE ItemID = {id}')
+            items = cursor.fetchall()
+        item = 'No such item with this id'
+        if len(items) >= 1:
+            item = {'item_id': items[0][0], 'description': items[0][1], 'quantity': items[0][2], 'supplier_id': items[0][3]}
+        return item
 
     app.run(port = 5002, debug = True)
+    DBConnection.get_instance().conn.close()
